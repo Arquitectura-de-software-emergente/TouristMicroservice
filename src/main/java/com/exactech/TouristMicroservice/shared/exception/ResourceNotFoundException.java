1718ab1b7666cadd9e7fcad718021ea7e06c7c7d
Shayla Choque <u20201c144@upc.edu.pe>
2024-05-22 22:45:32 -0500

    Implement shared classes

---
+++ src/main/java/com/exactech/TouristMicroservice/shared/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.exactech.TouristMicroservice.shared.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+        super();
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/com/exactech/TouristMicroservice/TouristMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exactech.TouristMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
