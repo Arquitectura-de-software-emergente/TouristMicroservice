eb43d76076f803dcbff11053ae15a38e49189f21
Shayla Choque <u20201c144@upc.edu.pe>
2024-05-22 22:44:20 -0500

    Add tourist application

---
+++ src/main/java/com/exactech/TouristMicroservice/TouristMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.exactech.TouristMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristMicroserviceApplication.class, args);
+	}
+
+}
